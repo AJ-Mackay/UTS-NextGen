8efd7e4958b8b519ae0a8b0fb8e17d2bd371a228
Ash Mackay <ash.mackay85@gmail.com>
2022-04-12 15:31:20 +0100

    Default Function Parameters

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ age = 30;
 
 // console.log(isOld);
 
-const add = (a: number, b: number) => a + b;
+const add = (a: number, b: number = 1) => a + b;
 
 const printOutput: (a: string | number) => void = (output) =>
   console.log(output);
@@ -27,4 +27,4 @@ if (button) {
   button.addEventListener("click", (event) => console.log(event));
 }
 
-printOutput(add(2, 5));
+printOutput(add(5));
