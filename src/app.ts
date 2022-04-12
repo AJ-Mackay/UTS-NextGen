5749c9194cb5f2a98922d98aee5f90ec68dacc22
Ash Mackay <ash.mackay85@gmail.com>
2022-04-12 15:29:26 +0100

    Arrow Functions

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,30 @@
-const userName = 'Ash';
+const userName = "Ash";
 // userName = "AJ";
 
 let age = 37;
 age = 30;
 
-function add(a: number, b: number) {
-    let result;    
-    result = a + b;
-    return result;
-}
+// function add(a: number, b: number) {
+//     let result;
+//     result = a + b;
+//     return result;
+// }
 
 // if (age > 20) {
 //     let isOld = true;
 // }
 
 // console.log(isOld);
+
+const add = (a: number, b: number) => a + b;
+
+const printOutput: (a: string | number) => void = (output) =>
+  console.log(output);
+
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", (event) => console.log(event));
+}
+
+printOutput(add(2, 5));
