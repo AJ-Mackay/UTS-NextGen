fcc301b147d5e94e93d5479a270c668bbc063a9d
Ash Mackay <ash.mackay85@gmail.com>
2022-04-12 16:00:53 +0100

    Array & Object Destructuring

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-const userName = "Ash";
-// userName = "AJ";
+// const userName = "Ash";
+// // userName = "AJ";
 
-let age = 37;
-age = 30;
+// let age = 37;
+// age = 30;
 
 // function add(a: number, b: number) {
 //     let result;
@@ -35,17 +35,27 @@ const activeHobbies = ["Cooking"];
 activeHobbies.push(...hobbies);
 
 const person = {
-  name: "Ash",
+  firstName: "Ash",
   age: 37,
 };
 
 const copiedPerson = { ...person };
 
 const add = (...numbers: number[]) => {
-    return numbers.reduce((curResult, curValue) => {
-        return curResult + curValue;
-    }, 0);
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
 };
 
 const addedNumbers = add(5, 10, 2, 3.7);
 console.log(addedNumbers);
+
+// const hobby1 = hobbies[0];
+// const hobby2 = hobbies[1];
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: userName, age } = person;
+
+console.log(userName, age);
