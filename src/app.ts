92fb2093d791216b10648bd55c9af7ea131c5342
Ash Mackay <ash.mackay85@gmail.com>
2022-04-12 15:20:17 +0100

    'let' and 'const'

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-// Code goes here!
+const userName = 'Ash';
+// userName = "AJ";
+
+let age = 37;
+age = 30;
+
+function add(a: number, b: number) {
+    let result;    
+    result = a + b;
+    return result;
+}
+
+// if (age > 20) {
+//     let isOld = true;
+// }
+
+// console.log(isOld);
